28250a776e9b6900171bdd4577d807b3423d4769
Wilton Silva <willtonnsilva@gmail.com>
2018-03-03 18:37:57 -0300

    adicionando um component de foto, onde o componet principal apenas passa paramentros para foto que ele quer invocar na view.

--- client/app/foto/foto.componet.ts
+++ client/app/foto/foto.componet.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
     selector:'foto',
     templateUrl:'./app/foto/foto.component.html'
 })
-export class FotoComponent {};
+export class FotoComponent {
+    @Input() src;
+    @Input() titulo;
+};
