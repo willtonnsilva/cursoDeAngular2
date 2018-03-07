54f3b60fdd379cf6a108b324513e24847bd09f9b
Wilton Silva <willtonnsilva@gmail.com>
2018-03-06 22:21:14 -0300

    Buscando as imagens da API REST e exibindo na view principal

--- client/app/app.componet.ts
+++ client/app/app.componet.ts
@@ -1,7 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, Inject } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
+    moduleId:module.id,
     selector:'app',
-    templateUrl: './app/app.componet.html'
+    templateUrl: './app.componet.html'
 })
-export class AppComponet {};
+export class AppComponet {
+
+    fotos : Array<Object> = [];
+
+    constructor(http:Http){
+        http.get('v1/fotos')
+        .map(res => res.json())
+        .subscribe(fotos => {
+            this.fotos = fotos;
+            console.log(this.fotos);
+        });
+    }
+};
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,10 +1,12 @@
+import 'rxjs/add/operator/map';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponet} from './app.componet';
 import {FotoModule} from './foto/foto.module'
+import {HttpModule} from '@angular/http';
 
 @NgModule({
-    imports:[BrowserModule, FotoModule],
+    imports:[BrowserModule, FotoModule, HttpModule],
     declarations: [AppComponet],
     bootstrap: [AppComponet]
 })
