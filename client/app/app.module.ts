138648c81dfa82c932b590aeb9949c0869719b7e
Wilton Silva <willtonnsilva@gmail.com>
2018-03-07 00:12:00 -0300

    Criando um painel para melhorar a experiência do usuário em visualizar as fotos.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,10 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponet} from './app.componet';
 import {FotoModule} from './foto/foto.module'
 import {HttpModule} from '@angular/http';
+import {PainelModule} from './painel/painel.module';
 
 @NgModule({
-    imports:[BrowserModule, FotoModule, HttpModule],
+    imports:[BrowserModule, FotoModule, HttpModule, PainelModule],
     declarations: [AppComponet],
     bootstrap: [AppComponet]
 })
-export class AppModule{}
+export class AppModule{}    
---
+++ client/app/painel/painel.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {PaineComponent} from './painel.component'
+
+@NgModule({
+    declarations:[PaineComponent],
+    exports: [PaineComponent]
+})
+export class PainelModule{}
